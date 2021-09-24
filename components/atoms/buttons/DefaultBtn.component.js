5efb7b37f1097e083e547b4f5d01f96054c6f1f0
jonathan-hetrafi <jonathan@hetrafi.com>
2021-09-24 23:15:56 +0200

    added styled components

--- components/atoms/buttons/DefaultBtn.component.js
+++ components/atoms/buttons/DefaultBtn.component.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { StyledDefaultBtn } from "./DefaultBtn.styles";
 
 export const DefaultBtn = React.forwardRef(({ onClickFn, href, text }, ref) => {
   const onClickAction = (e) => {
@@ -8,10 +9,12 @@ export const DefaultBtn = React.forwardRef(({ onClickFn, href, text }, ref) => {
   };
 
   return (
-    <Link href={href} passHref>
-      <a href={href} onClick={(e) => onClickAction(e)} ref={ref}>
-        {text}
-      </a>
-    </Link>
+    <StyledDefaultBtn>
+      <Link href={href} passHref>
+        <a href={href} onClick={(e) => onClickAction(e)} ref={ref}>
+          {text}
+        </a>
+      </Link>
+    </StyledDefaultBtn>
   );
 });
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
 import "../styles/globals.css";
 import { Provider } from "next-auth/client";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }) {
+  const theme = {
+    colors: {
+      primary: "#0070f3",
+    },
+  };
+
+  const GlobalStyles = createGlobalStyle`
+    body {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      background-color: #3ebbbb;
+      color: #eaeaea;
+    }
+  `;
+
   return (
     <Provider session={pageProps.session}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   );
 }
